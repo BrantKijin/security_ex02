80a7a74dbb2061c1342d7d92912a8469e94464bb
곽기진 <upper0house@gmail.com>
2025-01-06 16:29:17 +0900

    107

--- src/main/java/com/secure/notes/config/SecurityConfig.java
+++ src/main/java/com/secure/notes/config/SecurityConfig.java
@@ -7,15 +7,24 @@ import java.time.LocalDate;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
+import com.secure.notes.filter.CustomLoggingFilter;
+import com.secure.notes.filter.RequestValidationFilter;
 import com.secure.notes.model.AppRole;
 import com.secure.notes.model.Role;
 import com.secure.notes.model.User;
@@ -25,20 +34,44 @@ import com.secure.notes.repositories.UserRepository;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true,
+	securedEnabled = true,
+	jsr250Enabled = true)
 public class SecurityConfig {
 	@Bean
 	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+		http.csrf(csrf ->
+			csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+				.ignoringRequestMatchers("/api/auth/public/**")
+		);
+		//http.csrf(AbstractHttpConfigurer::disable);
 		http.authorizeHttpRequests((requests)
-			-> requests.anyRequest().authenticated());
-		http.csrf(AbstractHttpConfigurer::disable);
-		//http.formLogin(withDefaults());
+			-> requests
+			.requestMatchers("/api/admin/**").hasRole("ADMIN")
+			.requestMatchers("/api/csrf-token").permitAll()
+			.requestMatchers("/api/auth/public/**").permitAll()
+			.anyRequest().authenticated());
+		http.formLogin(withDefaults());
 		http.httpBasic(withDefaults());
 		return http.build();
 	}
 
 	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+		throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
 	public CommandLineRunner initData(RoleRepository roleRepository,
-		UserRepository userRepository) {
+		UserRepository userRepository,
+		PasswordEncoder passwordEncoder) {
 		return args -> {
 			Role userRole = roleRepository.findByRoleName(AppRole.ROLE_USER)
 				.orElseGet(() -> roleRepository.save(new Role(AppRole.ROLE_USER)));
@@ -47,7 +80,8 @@ public class SecurityConfig {
 				.orElseGet(() -> roleRepository.save(new Role(AppRole.ROLE_ADMIN)));
 
 			if (!userRepository.existsByUserName("user1")) {
-				User user1 = new User("user1", "user1@example.com", "{noop}password1");
+				User user1 = new User("user1", "user1@example.com",
+					passwordEncoder.encode("password1"));
 				user1.setAccountNonLocked(false);
 				user1.setAccountNonExpired(true);
 				user1.setCredentialsNonExpired(true);
@@ -61,7 +95,8 @@ public class SecurityConfig {
 			}
 
 			if (!userRepository.existsByUserName("admin")) {
-				User admin = new User("admin", "admin@example.com", "{noop}adminPass");
+				User admin = new User("admin", "admin@example.com",
+					passwordEncoder.encode("adminPass"));
 				admin.setAccountNonLocked(true);
 				admin.setAccountNonExpired(true);
 				admin.setCredentialsNonExpired(true);
@@ -76,3 +111,5 @@ public class SecurityConfig {
 		};
 	}
 }
+
+
